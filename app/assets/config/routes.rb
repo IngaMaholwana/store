eab5a3e7211bf23751d583fad1097257680132ae
Inga Maholwana <imaholwana023@student.wethinkcode.co.za>
2026-02-24 21:02:15 +0200

    what the main partss for routing in the applications in the web

--- app/assets/config/routes.rb
+++ app/assets/config/routes.rb
@@ -1,4 +1,15 @@
 Rails.application.routes.draw do
   # ...
   get "/products", to: "products#index"
+
+  get "/products/new", to: "products#new"
+  post "/products", to: "products#create"
+
+  get "/products/:id", to: "products#show"
+
+  get "/products/:id/edit", to: "products#edit"
+  patch "/products/:id", to: "products#update"
+  put "/products/:id", to: "products#update"
+
+  delete "/products/:id", to: "products#destroy"
 end
