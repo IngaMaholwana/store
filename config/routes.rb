0444eb833f252d705d5046ec47416755eb5b9463
Inga Maholwana <imaholwana023@student.wethinkcode.co.za>
2026-02-26 19:35:30 +0200

    adding uncormfirmed email to user and bit of email routes

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,7 @@ Rails.application.routes.draw do
   end
   resource :unsubscribe, only: [ :show ]
   namespace :settings do
+    resource :email, only: [ :show, :update ]
     resource :password, only: [ :show, :update ]
     resource :profile, only: [ :show, :update ]
     resource :user, only: [ :show, :destroy ]
